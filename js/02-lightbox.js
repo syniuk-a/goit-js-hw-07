819eb4996561c5798b64aca0b5cef9fc574f6e64
Andrii <78295684+syniuk-a@users.noreply.github.com>
2022-08-12 17:24:56 +0300

    added gallery 02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('ul.gallery');
+console.log('galleryEl => ', galleryEl);
+
+// создаём в памяти список изображений полученый из галереи
+const galleryList = galleryItems
+  .map(
+    ({ preview, original, description }) => `<div class="gallery__item" >
+  <a href="${original}" class="gallery__link">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    alt="${description}" 
+    data-source="${original}"
+    />
+</a>
+</div >`
+  )
+  .join('');
+
+// вывод галереи на экран
+galleryEl.insertAdjacentHTML('afterbegin', galleryList);
+// функционал слайдера (всплытие description)
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 150,
+});
